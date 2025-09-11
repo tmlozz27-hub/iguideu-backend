daee384830beaf655696c68908d7e1f6209a49c1
TU_USUARIO_REAL <TU_EMAIL_REAL@ejemplo.com>
2025-09-11 11:41:16 -0300

    fix(render): listen on 0.0.0.0 and use env PORT

--- server.js
+++ server.js
@@ -47,9 +47,10 @@ app.use((req, res) =>
 );
 
 // Listen
-app.listen(PORT, "127.0.0.1", () =>
-  console.log(`[OK] Servidor Express en 127.0.0.1:${PORT}`)
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`[OK] Servidor Express en 0.0.0.0:${PORT}`)
 );
+
 
 
 
